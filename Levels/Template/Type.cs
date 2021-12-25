395f459dbc096ac5d32324db3c130c44175db4ae
Mike McCabe <mikemccabe85@gmail.com>
2021-12-25 15:11:31 +0000

    init

--- Levels/Template/Type.cs
+++ Levels/Template/Type.cs
@@ -2,11 +2,12 @@ namespace Hopper
 {
     public enum Type
     {
-        Lily,
-        Goal,
-        Score,
-        Rock,
-        Water,
-        Jump
+        Lily,   // 0
+        Goal,   // 1
+        Score,  // 2
+        Rock,   // 3
+        Water,  // 4
+        Jump,   // 5
+        Bounce  // 6
     }
 }
