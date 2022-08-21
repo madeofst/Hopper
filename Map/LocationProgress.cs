2224100912ab569d82398d4c1d93512e8685e5b4
Mike McCabe <mikemccabe85@gmail.com>
2022-08-21 23:25:37 +0100

    tweak progress display and fix level access

--- Map/LocationProgress.cs
+++ Map/LocationProgress.cs
@@ -8,8 +8,6 @@ namespace Hopper
     {
         //Radius of circle for levelsprites
         const int Radius = 21;
-        //Offset for each individual sprite
-        //private Vector2 Offset = new Vector2(-4, -4);
 
         private List<Sprite> LevelSprites;
 
@@ -18,12 +16,12 @@ namespace Hopper
             LevelSprites = GetChildren().OfType<Sprite>().ToList();
             for(int i = 0; i < LevelSprites.Count; i++)
             {
-                float spacing = -Mathf.Tau / 2.7f / 5 * i;
-                float angularOffset = LevelSprites.Count / 2 + 0.15f;
-                float rotationAngle = spacing + angularOffset;
-                float ellipseWarpFactor = 1 - Mathf.Pow(Mathf.Sin(rotationAngle), 2f) * 0.15f;
+                float spacing = -Mathf.Tau / 2.7f / 5;
+                float angularOffset = -Mathf.Tau - (LevelSprites.Count * spacing / 2) + 1.34f;
+                float rotationAngle = angularOffset + spacing * i;
+                float ellipseWarpFactor = 1 - Mathf.Pow(Mathf.Sin(rotationAngle), 2f) * 0.2f;
                 Vector2 vec = new Vector2(Radius * ellipseWarpFactor, 0).Rotated(rotationAngle);
-                LevelSprites[i].Position = vec; //+ Offset;
+                LevelSprites[i].Position = vec;
             }
             Update(LevelReached);
         }
